7bdd68a52136d5576709ed1d8dd5ec0f32e7e189
Grant George <grantelliottgeorge@gmail.com>
2014-10-08 06:30:33 -0400

    first commit; basic project layout

---
+++ public/javascripts/services/posts.js
@@ -0,0 +1,42 @@
+angular.module('flapperNews')
+  .factory('posts', ['$http', function($http){
+    var o = {
+      posts: []
+    };
+    o.getAll = function() {
+      return $http.get('/posts').success(function(data){
+        angular.copy(data, o.posts);
+      });
+    };
+
+    o.create = function(post) {
+      return $http.post('/posts', post).success(function(data){
+        o.posts.push(data);
+      });
+    };
+
+    o.upvote = function(post) {
+      console.log(post);
+      return $http.put('/posts/' + post._id + '/upvote').success(function(data){
+        post.upvotes += 1;
+      });
+    };
+
+    o.get = function(id) {
+      return $http.get('/posts/' + id).then(function(res) {
+        return res.data;
+      });
+    };
+
+    o.addComment = function(id, comment) {
+      return $http.post('/posts/' + id + '/comments', comment);
+    }
+
+    o.upvoteComment = function(post, comment) {
+      return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote').success(function(data){
+        comment.upvotes += 1;
+      });
+    }
+
+    return o;
+  }]);
